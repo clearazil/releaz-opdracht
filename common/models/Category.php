a82196d86af08c470f6a8dca72c6bfd17dc02563
dvanderh <derkvanderheide@hotmail.com>
2016-11-01 17:41:51 +0100

    added categories model, controller and views

---
+++ common/models/Category.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\behaviors\TimestampBehavior;
+
+/**
+ * This is the model class for table "categories".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property integer $created_at
+ * @property integer $updated_at
+ *
+ * @property ActivityCategory[] $activityCategories
+ */
+class Category extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'categories';
+    }
+
+    public function behaviors()
+    {
+        return [
+            TimestampBehavior::className(),
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'name' => 'Name',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getActivityCategories()
+    {
+        return $this->hasMany(ActivityCategory::className(), ['category_id' => 'id']);
+    }
+}
